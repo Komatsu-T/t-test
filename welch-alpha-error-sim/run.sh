f5f981586d89ba27bcfff9d19948ff0a7e8dbba6
Komatsu-T <79096203+Komatsu-T@users.noreply.github.com>
2026-07-03 09:04:36 +0900

    Update run.sh

--- welch-alpha-error-sim/run.sh
+++ welch-alpha-error-sim/run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -d "t-test" ]; then
-  cd t-test
-  git pull
-  cd welch-alpha-error-sim
-else
-  git clone https://github.com/Komatsu-T/t-test.git
-  cd t-test/welch-alpha-error-sim
-fi
-
 sudo docker build -t welch-sim .
 sudo docker run -it --rm -v $(pwd)/output:/work/output welch-sim
 sudo chown -R $(whoami):$(whoami) output/
