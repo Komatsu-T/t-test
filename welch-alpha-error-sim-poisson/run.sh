6bf3025dee561982ecd9341eb5fcf212a337e38e
Komatsu-T <79096203+Komatsu-T@users.noreply.github.com>
2026-07-05 21:17:26 +0900

    Update run.sh

--- welch-alpha-error-sim-poisson/run.sh
+++ welch-alpha-error-sim-poisson/run.sh
@@ -2,7 +2,6 @@
 set -e
 
 sudo docker build -t welch-sim .
-tmux new -s sim
 sudo docker run -it --rm -v $(pwd)/output:/work/output welch-sim
 sudo chown -R $(whoami):$(whoami) output/
 aws s3 cp output/alpha_error_sim_poisson_result.parquet s3://welch-sim-test-komatsu-0112/alpha_error_sim_poisson_result.parquet
