d19d4e7423a8ab40ee34ed5dac41efd4935dcc38
Komatsu-T <79096203+Komatsu-T@users.noreply.github.com>
2026-07-07 13:46:16 +0900

    Update run.sh

--- welch-alpha-error-sim/run.sh
+++ welch-alpha-error-sim/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
+rm -rf output
 sudo docker build -t welch-sim .
 sudo docker run -it --rm -v $(pwd)/output:/work/output welch-sim
 sudo chown -R $(whoami):$(whoami) output/
-aws s3 cp output/alpha_error_sim_result.parquet s3://welch-sim-test-komatsu-0112/alpha_error_sim_result.parquet
+aws s3 cp output/ s3://welch-sim-test-komatsu-0112/ --recursive
 
 echo "完了：S3にアップロード済み"
